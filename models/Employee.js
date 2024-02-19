c78cca7bbf76bb3b715f6085c80b49c7930d1f2f
MustafizurRahman937 <mostafizurregina@gmail.com>
2024-02-18 22:04:32 -0500

    employee.js done

---
+++ models/Employee.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    gender: {
+        type: String,
+        enum: ['Male', 'Female', 'Other'],
+    },
+    salary: {
+        type: Number,
+        required: true,
+    },
+});
+
+const Employee = mongoose.model('Employee', employeeSchema);
+
+module.exports = Employee;
